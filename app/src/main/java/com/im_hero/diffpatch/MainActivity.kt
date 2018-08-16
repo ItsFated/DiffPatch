cd2e030b60d534e2a7e655b88a674c04b0f985f7
Jason <384378433@qq.com>
2018-08-16 10:37:32 +0800

    添加运行时权限请求

--- app/src/main/java/com/im_hero/diffpatch/MainActivity.kt
+++ app/src/main/java/com/im_hero/diffpatch/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.im_hero.diffpatch
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
+import com.im_hero.diffpatch.java.Permissions
 
 class MainActivity : AppCompatActivity() {
+    lateinit var permissions: Permissions
+    lateinit var permissionsHandler: PermissionsHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Example of a call to a native method
-        sample_text.text = ""
+        permissionsHandler = PermissionsHandler()
+        permissions = Permissions(this)
+                .setDefaultPermissionsDenied(permissionsHandler)
+                .setDefaultPermissionsGranted(permissionsHandler)
+                .setDefaultPermissionsShowRationale(permissionsHandler)
+        permissions.request(Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
     }
 
+    inner class PermissionsHandler: Permissions.PermissionsDenied, Permissions.PermissionsShowRationale, Permissions.PermissionsGranted{
+        override fun onPermissionsGranted(permissions: Array<out String>?) {}
+        override fun onPermissionsDenied(permissions: Array<out String>?): Boolean = true
+        override fun onPermissionsShowRationale(permissions: Array<out String>?): Boolean = true
+    }
 }
