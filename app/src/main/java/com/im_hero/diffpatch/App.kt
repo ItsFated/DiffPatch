9d3f0e78982b5052fcc2992ff92593d5a8c14f8d
Jason <384378433@qq.com>
2018-08-18 11:40:28 +0800

    添加通过URI获取文件路径的方法

--- app/src/main/java/com/im_hero/diffpatch/App.kt
+++ app/src/main/java/com/im_hero/diffpatch/App.kt
@@ -2,6 +2,10 @@ package com.im_hero.diffpatch
 
 import android.app.Application
 import android.util.Log
+import android.provider.MediaStore
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
 
 class App : Application() {
 
@@ -21,6 +25,27 @@ class App : Application() {
             private set
         var versionName: String = ""
             private set
-    }
 
+        fun getRealFilePath(context: Context, uri: Uri): String? {
+            val scheme = uri.scheme
+            var data: String? = null
+            if (scheme == null)
+                data = uri.path
+            else if (ContentResolver.SCHEME_FILE == scheme) {
+                data = uri.path
+            } else if (ContentResolver.SCHEME_CONTENT == scheme) {
+                val cursor = context.contentResolver.query(uri, arrayOf(MediaStore.Images.ImageColumns.DATA), null, null, null)
+                if (null != cursor) {
+                    if (cursor.moveToFirst()) {
+                        val index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+                        if (index > -1) {
+                            data = cursor.getString(index)
+                        }
+                    }
+                    cursor.close()
+                }
+            }
+            return data
+        }
+    }
 }
